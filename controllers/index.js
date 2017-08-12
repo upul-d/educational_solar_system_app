3f92b741e5064708fe60ae39bc8a4d19bc84a862
upuld <u.dissanayaka@gmail.com>
2017-08-12 11:46:04 +0100

    Added webpack & modified file structure

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = new express.Router();
 var mongoClient = require("mongodb").MongoClient;
-var ObjectID = require('mongodb').ObjectID;
 
 
 router.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,3 +10,5 @@ app.use(express.static('client/build'));
 app.listen(3000, function() {
   console.log('App running on port ' + this.address().port);
 });
+
+app.use(require("./controllers/index.js"));
