db1fe5b939f6b967531285e67cdee5bcf3738852
Filipe <granha.carlos@gmail.com>
2017-08-13 19:34:27 +0100

    added venus image

--- client/src/views/draw_canvas.js
+++ client/src/views/draw_canvas.js
@@ -13,6 +13,19 @@ DrawCanvas.prototype.render = function() {
   var sun = new Square(this.canvas, -55*this.scale, -55*this.scale, 110*this.scale, 110*this.scale, "transparent", "transparent");
   sun.img = "images/sun.png";
   sun.drawImg();
+
+  var mercury = new Square(this.canvas, -65*this.scale, 65*this.scale, 20*this.scale, 20*this.scale, "transparent", "transparent");
+  mercury.img = "images/mercury.png";
+  mercury.drawImg();
+
+  var venus = new Square(this.canvas, 90*this.scale, 90*this.scale, 30*this.scale, 30*this.scale, "transparent", "transparent");
+  venus.img = "images/venus.png";
+  venus.drawImg();
+
+
+
+
+
 }
 
 
