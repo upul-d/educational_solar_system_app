ef37cdb57a8a8e3a75e3ce66c70672368e7b66b4
Filipe <granha.carlos@gmail.com>
2017-08-13 17:29:41 +0100

    Added Venus to api object

--- planet_api.js
+++ planet_api.js
@@ -53,51 +53,52 @@ var mercury = {
 }
 
 
-// var venus = {
-//   "Name": "Mercury",
+var venus = {
+  "Name": "Venus",
 
-//   "breadCrumbs": [
-//   {
-//     "label": "Solar System",
-//     "link": "localHost:3000/route-to-solar-system-page"
-//   }
-//   ],
+  "breadCrumbs": [
+  {
+    "label": "Solar System",
+    "link": "localHost:3000/route-to-solar-system-page"
+  }
+  ],
   
-//   "links": [],
+  "links": [],
 
-//   "mainImage": {
-//     "label": "Mercury",
-//     "link": "image-link-to-go-here"
-//   },
+  "mainImage": {
+    "label": "Venus",
+    "link": "image-link-to-go-here"
+  },
 
-//   "sideBar": [
-//   {
-//     "label": "Radius",
-//     "value": "2439.7" + "km"
-//   },
-//   {
-//     "label": "temperatureCelsius",
-//     "value": 167
-//   }],
+  "sideBar": [
+  {
+    "label": "Radius",
+    "value": "6051.8" + "km"
+  },
+  {
+    "label": "temperatureCelsius",
+    "value": 464
+  }],
 
-//   "description": [
-//   {
-//     "type": "paragraph",
-//     "title": "info on mercury",
-//     "value": "info on mercury"
-//   }],
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on venus",
+    "value": "info on venus"
+  }],
 
-//   "gallery": [
-//   {
-//     "label": "image 1",
-//     "lowRes": "link for low-res image",
-//     "hiRes": "link for hi-res image"
-//   }],
+  "gallery": [
+  {
+    "label": "image 1",
+    "lowRes": "link for low-res image",
+    "hiRes": "link for hi-res image"
+  }],
 
-//   "facts": ["fact 1", "fact 2", "fact 3"]
+  "facts": ["fact 1", "fact 2", "fact 3"]
 
-// }
+}
 
 
 planets.push(mercury);
+planets.push(venus);
 db.planets.insert(solarSystem);
