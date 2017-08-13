26097f27fd4a2ccc132f04fa817d243624790884
Filipe <granha.carlos@gmail.com>
2017-08-13 16:13:41 +0100

    Added get planet by index route

--- controllers/api.js
+++ controllers/api.js
@@ -13,7 +13,13 @@ mongoClient.connect("mongodb://localhost:27017/solarSystem", function(err, db){
     db.collection("planets").find().toArray(function(err, result){
       res.json(result[0].planets);
     });
-  })
+  });
+
+  apiRouter.get("/planets/:id", function(req, res){
+    db.collection("planets").find().toArray(function(err, result){
+      res.json(result[0].planets[req.params.id]);
+    });
+  });
 
   console.log("Connected to database");
 })
