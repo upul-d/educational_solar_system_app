b87a7da9cf100c33f8c2fa142413faa937f959af
Filipe <granha.carlos@gmail.com>
2017-08-13 18:15:48 +0100

    Added earth to api

--- planet_api.js
+++ planet_api.js
@@ -98,7 +98,58 @@ var venus = {
 
 }
 
+var earth = {
+  "Name": "Earth",
+
+  "breadCrumbs": [
+  {
+    "label": "Solar System",
+    "link": "localHost:3000/route-to-solar-system-page"
+  }
+  ],
+  
+  "links": [
+    {
+      "label": "Moon",
+      "link": "route to moon "
+    }
+  ],
+
+  "mainImage": {
+    "label": "Earth",
+    "link": "image-link-to-go-here"
+  },
+
+  "sideBar": [
+    {
+      "label": "Radius",
+      "value": "6378.1 km"
+    },
+    {
+      "label": "Average Temperature In Celsius",
+      "value": 16
+    }
+  ],
+
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on earth",
+    "value": "info on earth"
+  }],
+
+  "gallery": [
+  {
+    "label": "image 1",
+    "lowRes": "link for low-res image",
+    "hiRes": "link for hi-res image"
+  }],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+
+}
 
 planets.push(mercury);
 planets.push(venus);
+planets.push(earth);
 db.planets.insert(solarSystem);
