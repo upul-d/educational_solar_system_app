0ab91aea09fbde7cccb21155b4bcbbe4c866d9b1
James Cox <j-cox03@hotmail.com>
2017-08-14 05:54:45 +0100

    Made saturn element for api

---
+++ api_single_elements/saturn.js
@@ -0,0 +1,49 @@
+var Saturn = {
+  "Name": "Saturn",
+
+  "breadCrumbs": [
+    {
+      "label": "Solar System",
+      "link": "localHost:3000/route-to-solar-system-page"
+    }
+  ],
+
+  "links": [],
+
+  "mainImage": {
+    "label": "Saturn",
+    "link": "image-link-to-go-here"
+  },
+
+  "sideBar": [
+    {
+      "label": "Length Of A Day",
+      "value": "10.6 hours"
+    },
+    {
+      "label": "Average Temperature In Celsius",
+      "value": -178
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "1.429 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on earth",
+    "value": "info on earth"
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "link for low-res image",
+      "hiRes": "link for hi-res image"
+    }
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
