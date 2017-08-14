eb161ebad01d97192930a5674ae6fa43e98fb4fa
James Cox <j-cox03@hotmail.com>
2017-08-14 06:20:23 +0100

    made kerberos file and added to api object

--- planet_api_temp.js
+++ planet_api_temp.js
@@ -52,7 +52,6 @@ var mercury = {
 
 }
 
-
 var venus = {
   "Name": "Venus",
 
@@ -148,6 +147,66 @@ var earth = {
   "facts": ["fact 1", "fact 2", "fact 3"]
 }
 
+var uranus = {
+  "Name": "Uranus",
+
+  "breadCrumbs": [
+    {
+      "label": "Solar System",
+      "link": "localHost:3000/route-to-solar-system-page"
+    }
+  ],
+
+  "links": [],
+
+  "mainImage": {
+    "label": "Uranus",
+    "link": "image-link-to-go-here"
+  },
+
+  "sideBar": [
+    {
+      "label": "Length Of A Day",
+      "value": "17 hours"
+    },
+    {
+      "label": "Average Temperature In Celsius",
+      "value": -357
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "2.871 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on Uranus",
+    "value": "Uranus is the seventh planet from the Sun. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. Uranus is similar in composition to Neptune, and both have different bulk chemical composition from that of the larger gas giants Jupiter and Saturn. For this reason, scientists often classify Uranus and Neptune as 'ice giants' to distinguish them from the gas giants. Uranus's atmosphere is similar to Jupiter's and Saturn's in its primary composition of hydrogen and helium, but it contains more 'ices' such as water, ammonia, and methane, along with traces of other hydrocarbons. It is the coldest planetary atmosphere in the Solar System, with a minimum temperature of 49 K (−224 °C; −371 °F), and has a complex, layered cloud structure with water thought to make up the lowest clouds and methane the uppermost layer of clouds. The interior of Uranus is mainly composed of ices and rock."
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "images/uranus1.png",
+      "hiRes": "images/uranus1.png"
+    },
+    {
+      "label": "image 2",
+      "lowRes": "images/uranus2.png",
+      "hiRes": "images/uranus2.png"
+    },
+    {
+      "label": "image 3",
+      "lowRes": "images/uranus3.png",
+      "hiRes": "images/uranus3.png"
+    },
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
+
 var pluto = {
   "Name": "Pluto",
 
@@ -222,9 +281,9 @@ var pluto = {
   "facts": ["fact 1", "fact 2", "fact 3"]
 }
 
-
 planets.push(mercury);
 planets.push(venus);
 planets.push(earth);
+planets.push(uranus);
 planets.push(pluto);
 db.solarSystem.insert(solarSystem);
