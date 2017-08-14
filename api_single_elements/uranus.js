0a63f0b3d2fc71082969c664f0725c876c39de14
James Cox <j-cox03@hotmail.com>
2017-08-14 05:58:16 +0100

    Made uranus element for api

---
+++ api_single_elements/uranus.js
@@ -0,0 +1,49 @@
+var uranus = {
+  "Name": "Uranus",
+
+  "breadCrumbs": [
+    {
+      "label": "Solar System",
+      "link": "localHost:3000/route-to-solar-system-page"
+    }
+  ],
+
+  "links": [],
+
+  "mainImage": {
+    "label": "Uranus",
+    "link": "image-link-to-go-here"
+  },
+
+  "sideBar": [
+    {
+      "label": "Length Of A Day",
+      "value": "9.9 hours"
+    },
+    {
+      "label": "Average Temperature In Celsius",
+      "value": -224
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "2.871 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on earth",
+    "value": "info on earth"
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "link for low-res image",
+      "hiRes": "link for hi-res image"
+    }
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
