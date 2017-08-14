8f6ac7af0301449b596ffed57f2bf0c14a2702fb
James Cox <j-cox03@hotmail.com>
2017-08-14 06:15:06 +0100

    Added to temp api file

--- api_single_elements/uranus.js
+++ api_single_elements/uranus.js
@@ -18,11 +18,11 @@ var uranus = {
   "sideBar": [
     {
       "label": "Length Of A Day",
-      "value": "9.9 hours"
+      "value": "17 hours"
     },
     {
       "label": "Average Temperature In Celsius",
-      "value": -224
+      "value": -357
     },
     {
       "label": "Distance From The Sun",
@@ -33,16 +33,26 @@ var uranus = {
   "description": [
   {
     "type": "paragraph",
-    "title": "info on earth",
-    "value": "info on earth"
+    "title": "info on Uranus",
+    "value": "Uranus is the seventh planet from the Sun. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. Uranus is similar in composition to Neptune, and both have different bulk chemical composition from that of the larger gas giants Jupiter and Saturn. For this reason, scientists often classify Uranus and Neptune as 'ice giants' to distinguish them from the gas giants. Uranus's atmosphere is similar to Jupiter's and Saturn's in its primary composition of hydrogen and helium, but it contains more 'ices' such as water, ammonia, and methane, along with traces of other hydrocarbons. It is the coldest planetary atmosphere in the Solar System, with a minimum temperature of 49 K (−224 °C; −371 °F), and has a complex, layered cloud structure with water thought to make up the lowest clouds and methane the uppermost layer of clouds. The interior of Uranus is mainly composed of ices and rock."
   }],
 
   "gallery": [
     {
       "label": "image 1",
-      "lowRes": "link for low-res image",
-      "hiRes": "link for hi-res image"
-    }
+      "lowRes": "images/uranus1.png",
+      "hiRes": "images/uranus1.png"
+    },
+    {
+      "label": "image 2",
+      "lowRes": "images/uranus2.png",
+      "hiRes": "images/uranus2.png"
+    },
+    {
+      "label": "image 3",
+      "lowRes": "images/uranus3.png",
+      "hiRes": "images/uranus3.png"
+    },
   ],
 
   "facts": ["fact 1", "fact 2", "fact 3"]
