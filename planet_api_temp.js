b5cb6cb52d658e47736629a084d9e11cd0653781
James Cox <j-cox03@hotmail.com>
2017-08-14 06:28:10 +0100

    Made ariel element and added to api object

--- planet_api_temp.js
+++ planet_api_temp.js
@@ -7,6 +7,7 @@ var planets = [];
 
 solarSystem.planets = planets;
 
+// PLANETS ------------------------------------------
 var mercury = {
   "Name": "Mercury",
 
@@ -281,9 +282,73 @@ var pluto = {
   "facts": ["fact 1", "fact 2", "fact 3"]
 }
 
+// PLUTO MOONS ------------------------------------------
+
+pluto.moons = []
+
+var kerberos = {
+  "Name": "Kerberos",
+
+  "breadCrumbs": [
+    {
+      "label": "Solar System",
+      "link": "localHost:3000/route-to-solar-system-page"
+    },
+    {
+      "label": "Pluto",
+      "link": "localHost:3000/route-to-pluto-page"
+    },
+  ],
+
+  "links": [],
+
+  "mainImage": {
+    "label": "Kerberos",
+    "link": "image-link-to-go-here"
+  },
+
+  "sideBar": [
+    {
+      "label": "Radius",
+      "value": "606km"
+    },
+    {
+      "label": "Orbit Radius",
+      "value": "19,640 km"
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "5.9 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on Kerberos",
+    "value": "Kerberos is a small natural satellite of Pluto, about 12 km (7.5 mi) in its longest dimension. It was the fourth moon of Pluto to be discovered and its existence was announced on 20 July 2011. It was imaged, along with Pluto and its four other moons, by the New Horizons spacecraft in July 2015. The first image of Kerberos was released to the public on 22 October 2015. Kerberos appears to be smaller than scientists expected and has a highly-reflective surface, counter to predictions prior to the Pluto flyby in July. “Once again, the Pluto system has surprised us,” said New Horizons Project Scientist Hal Weaver, of the Johns Hopkins University Applied Physics Laboratory in Laurel, Maryland. The new data, downlinked from the New Horizons spacecraft on Oct. 20, show that Kerberos appears to have a double-lobed shape, with the larger lobe approximately 5 miles (8 kilometers) across and the smaller lobe approximately 3 miles (5 kilometers) across. Science team members speculate from its unusual shape that Kerberos could have been formed by the merger of two smaller objects. The reflectivity of Kerberos’ surface is similar to that of Pluto’s other small moons (approximately 50 percent) and strongly suggests Kerberos, like the others, is coated with relatively clean water ice.",
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "link for low-res image",
+      "hiRes": "link for hi-res image"
+    }
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
+
 planets.push(mercury);
+
 planets.push(venus);
+
 planets.push(earth);
+
 planets.push(uranus);
+
 planets.push(pluto);
+pluto.moons.push(kerberos);
+
 db.solarSystem.insert(solarSystem);
