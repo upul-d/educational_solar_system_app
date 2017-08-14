44ccc60028dab01f75222ec9f6afac8de02450f2
Eugene Kim <eugenekim.edi@gmail.com>
2017-08-14 16:19:18 +0100

    Pushing moons api

---
+++ api_single_elements/neptune_moons/triton.js
@@ -0,0 +1,40 @@
+var triton = {
+  "Name": "Triton",
+
+  "breadCrumbs": [],
+
+  "links": [],
+
+  "mainImage": {},
+
+  "sideBar": [
+    {
+      "label": "Radius",
+      "value": "1353.4 km"
+    },
+    {
+      "label": "Orbit Radius",
+      "value": "354,760 km"
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "4.5 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "title": "info on Triton",
+    "value": "Triton is the largest natural satellite of the planet Neptune. It was discovered on October 10, 1846, by English astronomer William Lassell. It is the only large moon in the Solar System with a retrograde orbit, an orbit in the opposite direction to its planet's rotation.[2][11] At 2,700 kilometres (1,700 mi) in diameter, it is the seventh-largest moon in the Solar System. Because of its retrograde orbit and composition similar to Pluto's, Triton is thought to have been a dwarf planet captured from the Kuiper belt.[12] Triton has a surface of mostly frozen nitrogen, a mostly water-ice crust,[13] an icy mantle and a substantial core of rock and metal. The core makes up two-thirds of its total mass. Triton has a mean density of 2.061 g/cm3 and is composed of approximately 15–35% water ice. Triton is one of the few moons in the Solar System known to be geologically active (the others being Jupiter's Io and Saturn's Enceladus). As a consequence, its surface is relatively young with sparse impact craters, and a complex geological history revealed in intricate cryovolcanic and tectonic terrains. Part of its surface has geysers erupting sublimated nitrogen gas, contributing to a tenuous nitrogen atmosphere less than 1/70,000 the pressure of Earth's atmosphere at sea level.",
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "./images/neptune/triton/triton.jpg",
+      "hiRes": "./images/neptune/triton/triton.jpg"
+    }
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
---
+++ api_single_elements/uranus_moons/titania.js
@@ -0,0 +1,40 @@
+var titania = {
+  "Name": "Titania",
+
+  "breadCrumbs": [],
+
+  "links": [],
+
+  "mainImage": {},
+
+  "sideBar": [
+    {
+      "label": "Radius",
+      "value": "788 km"
+    },
+    {
+      "label": "Orbit Radius",
+      "value": "436,000 km"
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "2.8 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "title": "info on Titania",
+    "value": "Titania is the largest of the moons of Uranus and the eighth largest moon in the Solar System at a diameter of 1,578 kilometres (981 mi). Discovered by William Herschel in 1787, Titania is named after the queen of the fairies in Shakespeare's A Midsummer Night's Dream. Its orbit lies inside Uranus's magnetosphere. Titania consists of approximately equal amounts of ice and rock, and is probably differentiated into a rocky core and an icy mantle. A layer of liquid water may be present at the core–mantle boundary. The surface of Titania, which is relatively dark and slightly red in color, appears to have been shaped by both impacts and endogenic processes.",
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "./images/uranus/titania/Titania.jpg",
+      "hiRes": "./images/uranus/titania/Titania.jpg"
+    }
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
