f2cea36fadcdebb9d4629c327f01c2d3037bf5ac
James Cox <j-cox03@hotmail.com>
2017-08-14 08:58:31 +0100

    Added moon hyperion to api object

--- planet_api_temp.js
+++ planet_api_temp.js
@@ -496,6 +496,62 @@ var moon = {
 
 // SATURN MOONS ------------------------------------------
 
+saturn.moons = [];
+
+var hyperion = {
+  "Name": "Hyperion",
+
+  "breadCrumbs": [
+    {
+      "label": "Solar System",
+      "link": "localHost:3000/route-to-solar-system-page"
+    },
+    {
+      "label": "Saturn",
+      "link": "localHost:3000/route-to-saturn-page"
+    },
+  ],
+
+  "links": [],
+
+  "mainImage": {
+    "label": "Hyperion",
+    "link": "image-link-to-go-here"
+  },
+
+  "sideBar": [
+    {
+      "label": "Radius",
+      "value": "135 km"
+    },
+    {
+      "label": "Orbit Radius",
+      "value": "1,500,934 km"
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "1.429 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on Kerberos",
+    "value": "Ariel is the fourth-largest of the 27 known moons of Uranus. Ariel orbits and rotates in the equatorial plane of Uranus, which is almost perpendicular to the orbit of Uranus, and so has an extreme seasonal cycle. It was discovered in October 1851 by William Lassell, and named for a character in two different pieces of literature. As of 2012, much of the detailed knowledge of Ariel derives from a single flyby of Uranus performed by the spacecraft Voyager 2 in 1986, which managed to image around 35% of the moon's surface. There are no active plans at present to return to study the moon in more detail, although various concepts such as a Uranus orbiter and probe have been proposed from time to time. After Miranda, Ariel is the second-smallest of Uranus' five major rounded satellites, and the second-closest to its planet. Among the smallest of the Solar System's 19 known spherical moons (it ranks 14th among them in diameter), it is believed to be composed of roughly equal parts ice and rocky material. Its mass is approximately equal in magnitude to Earth's hydrosphere.",
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "image/ariel1.jpg",
+      "hiRes": "image/ariel1.jpg"
+    }
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
+
 // URANUS MOONS ------------------------------------------
 
 uranus.moons = [];
@@ -626,6 +682,7 @@ planets.push(mars);
 planets.push(jupiter);
 
 planets.push(saturn);
+saturn.moons.push(hyperion);
 
 planets.push(uranus);
 uranus.moons.push(ariel);
