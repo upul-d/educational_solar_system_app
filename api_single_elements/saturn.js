2cd804710c9a106738c8cd90b70f1bed5fd7ebe1
James Cox <j-cox03@hotmail.com>
2017-08-14 06:33:22 +0100

    Added saturn to api object

--- api_single_elements/saturn.js
+++ api_single_elements/saturn.js
@@ -33,8 +33,8 @@ var saturn = {
   "description": [
   {
     "type": "paragraph",
-    "title": "info on earth",
-    "value": "info on earth"
+    "title": "info on saturn",
+    "value": "The second largest planet in our solar system, adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings -- made of chunks of ice and rock -- but none are as spectacular or as complicated as Saturn's. Like fellow gas giant Jupiter, Saturn is a massive ball of mostly hydrogen and helium. Surrounding by 53 confirmed and nine provisional moons, Saturn is home to some of the most fascinating landscapes in our solar system. From the jets of Enceladus to the methane lakes on smoggy Titan, the Saturn system is a rich source of scientific discovery and still holds many mysteries. The farthest planet from Earth observable by the unaided human eye, Saturn has been known since ancient times and is named for the Roman god of agriculture and wealth. The Greek equivalent was Cronos, the father of Zeus/Jupiter.",
   }],
 
   "gallery": [
--- planet_api_temp.js
+++ planet_api_temp.js
@@ -148,6 +148,56 @@ var earth = {
   "facts": ["fact 1", "fact 2", "fact 3"]
 }
 
+var saturn = {
+  "Name": "Saturn",
+
+  "breadCrumbs": [
+    {
+      "label": "Solar System",
+      "link": "localHost:3000/route-to-solar-system-page"
+    }
+  ],
+
+  "links": [],
+
+  "mainImage": {
+    "label": "Saturn",
+    "link": "image-link-to-go-here"
+  },
+
+  "sideBar": [
+    {
+      "label": "Length Of A Day",
+      "value": "10.6 hours"
+    },
+    {
+      "label": "Average Temperature In Celsius",
+      "value": -178
+    },
+    {
+      "label": "Distance From The Sun",
+      "value": "1.429 billion km"
+    }
+  ],
+
+  "description": [
+  {
+    "type": "paragraph",
+    "title": "info on saturn",
+    "value": "The second largest planet in our solar system, adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings -- made of chunks of ice and rock -- but none are as spectacular or as complicated as Saturn's. Like fellow gas giant Jupiter, Saturn is a massive ball of mostly hydrogen and helium. Surrounding by 53 confirmed and nine provisional moons, Saturn is home to some of the most fascinating landscapes in our solar system. From the jets of Enceladus to the methane lakes on smoggy Titan, the Saturn system is a rich source of scientific discovery and still holds many mysteries. The farthest planet from Earth observable by the unaided human eye, Saturn has been known since ancient times and is named for the Roman god of agriculture and wealth. The Greek equivalent was Cronos, the father of Zeus/Jupiter.",
+  }],
+
+  "gallery": [
+    {
+      "label": "image 1",
+      "lowRes": "link for low-res image",
+      "hiRes": "link for hi-res image"
+    }
+  ],
+
+  "facts": ["fact 1", "fact 2", "fact 3"]
+}
+
 var uranus = {
   "Name": "Uranus",
 
@@ -403,6 +453,8 @@ planets.push(mercury);
 planets.push(venus);
 
 planets.push(earth);
+
+planets.push(saturn);
 
 planets.push(uranus);
 uranus.moons.push(ariel);
