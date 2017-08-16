4dd8e6696c7f06eac6588b75451399db15ac1921
James Cox <j-cox03@hotmail.com>
2017-08-16 13:45:59 +0100

    working on background

--- client/src/app.js
+++ client/src/app.js
@@ -40,7 +40,67 @@ var manageBackgroundCanvas = function(){
   resizeCanvas();
 }
 
+var starField = function(){
+  MAX_DEPTH = 40; // controls how far away are the stars created in space
+
+   var canvas, ctx;
+   var stars = new Array(200);
+
+    canvas = document.getElementById("star-field");
+    if( canvas && canvas.getContext ) {
+      ctx = canvas.getContext("2d");
+      initStars();
+      setInterval(loop,37); // sets the speed of looping
+     }
+
+  function randomRange(minVal,maxVal) {
+    return Math.floor(Math.random() * (maxVal - minVal -1)) + minVal;
+  }
+
+  function initStars() {
+    for( var i = 0; i < stars.length; i++ ) {
+      stars[i] = {
+        x: randomRange(-30,30),
+        y: randomRange(-30,30),
+        z: randomRange(20,MAX_DEPTH)
+       }
+    }
+  }
+
+  function loop() {
+    var halfWidth  = canvas.width/5;
+    var halfHeight = canvas.height/5;
+
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    for( var i = 0; i < stars.length; i++ ) {
+      stars[i].z -= 0.2; // sets the speed of the stars
+
+      if( stars[i].z <= 0 ) {
+        stars[i].x = randomRange(-25,25);
+        stars[i].y = randomRange(-25,25);
+        stars[i].z = MAX_DEPTH;
+      }
+
+      var k  = 128.0 / stars[i].z;
+      var px = stars[i].x * k + halfWidth;
+      var py = stars[i].y * k + halfHeight;
+
+      if( px >= 0 && px <= 500 && py >= 0 && py <= 400 ) {
+        var size = 1; // size of the stars
+        var shade = parseInt((1 - stars[i].z / 128.0) * 255);
+        ctx.fillStyle = "rgb(" + shade + "," + shade + "," + shade + ")";
+        ctx.fillRect(px,py,size,size);
+      }
+    }
+  }
+}
+
 window.addEventListener('load', function() {
+
+  // starField();
+
   var storedFavourites = localStorage.getItem("favourites");
   if(!storedFavourites){
     var emptyArray = JSON.stringify([]);
